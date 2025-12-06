25bd7a658502a2b8c706617de6f5db87ee6a1da4
eren alemdar <63112553+Ernalmdr@users.noreply.github.com>
2025-12-07 01:45:15 +0300

    Revert "business katmanı"

--- GamerBox.BusinessLayer/Abstract/IPostService.cs
+++ GamerBox.BusinessLayer/Abstract/IPostService.cs
@@ -1,6 +1,10 @@
-
+﻿using GamerBox.EntitiesLayer.Concrete;
+using System;
 using System.Collections.Generic;
-
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 using GamerBox.EntitiesLayer.Concrete;
 
 namespace GamerBox.BusinessLayer.Abstract
